3bc8f7706160efaeec480ad9d5ab4673fe82b7c3
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-10-14 16:24:52 +0300

    arr

---
+++ js/array.js
@@ -0,0 +1,15 @@
+const gameUHD = [
+    {name: 'Far Cry 6', year: '2021', 
+            video: ['zGMzdiVq7lE', 'Z6d5a3IDKqE', 'Gw2HhTmmzjU', 'VxSNPe6-mts', 
+                    'wZhsduP4CHM', '43lEGhlYEdI', 'fN4ZmUqyDvA']},
+    {name: 'Survarium', year: '2021', video: ['uRQwR0TlLaI',]},
+    {name: '', year: '', video: []},
+]
+ 
+// for (let i = 0; i < gameUHD.length; i++){
+//     console.log(gameUHD[i]);
+// }
+
+gameUHD.forEach (function(person, index, gameUHD ){
+    console.log(person)
+})
