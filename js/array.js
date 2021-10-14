41f8aaf8f1b7048e2b7f428729c61b2734741d03
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-10-14 17:45:07 +0300

    filter

--- js/array.js
+++ js/array.js
@@ -3,6 +3,7 @@ const gameUHD = [
             video: ['zGMzdiVq7lE', 'Z6d5a3IDKqE', 'Gw2HhTmmzjU', 'VxSNPe6-mts', 
                     'wZhsduP4CHM', '43lEGhlYEdI', 'fN4ZmUqyDvA']},
     {name: 'Survarium', year: '2021', video: ['uRQwR0TlLaI',]},
+    {name: 'Call of Duty: Black Ops Cold War', year: '2020', video: ['Yv3Sfz-87Wk',]},
     {name: '', year: '', video: []},
 ]
  
@@ -10,6 +11,21 @@ const gameUHD = [
 //     console.log(gameUHD[i]);
 // }
 
-gameUHD.forEach (function(person, index, gameUHD ){
-    console.log(person)
-})
+// gameUHD.forEach (function(person, index, gameUHD ){
+//     console.log(person);
+// })
+
+// gameUHD.forEach (person => console.log (person))
+
+// const newGameUHD = gameUHD.map(person => `${person.name} (${person.video})`)
+// console.log(newGameUHD);
+
+// console.log(gameUHD);
+// const filtArr = []
+// for (let i = 0; i < gameUHD.length; i++){
+//     if (gameUHD[i].year <= 2020) {
+//         filtArr.push(gameUHD[i])
+//     }
+// }
+// console.log(filtArr);
+
