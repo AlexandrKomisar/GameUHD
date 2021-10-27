8a9b30aae05964b9aa533baa7ceabbc1d167093a
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-10-27 14:44:31 +0300

    005

--- js/array.js
+++ js/array.js
@@ -1,7 +1,7 @@
-const gameUHD = [
+let gameUHD = [
     {name: 'Far Cry 6', year: '2021', genre: 'action', 
             video: ['zGMzdiVq7lE', 'Z6d5a3IDKqE', 'Gw2HhTmmzjU', 'VxSNPe6-mts', 
-                    'wZhsduP4CHM', '43lEGhlYEdI', 'fN4ZmUqyDvA']},
+                    'wZhsduP4CHM', '43lEGhlYEdI', 'fN4ZmUqyDvA', '2R3Iy0rFTAg']},
     {name: 'Survarium', year: '2021', genre: 'online action', video: ['uRQwR0TlLaI',]},
     {name: 'Call of Duty: Black Ops Cold War', year: '2020', genre: 'action', video: ['Yv3Sfz-87Wk',]},
     {name: '', year: '', genre: '', video: []},
